7b7e27e751fcd59daf0ba3746ee479916d3680d9
Ayushmaan218 <121386512+Ayushmaan218@users.noreply.github.com>
2025-09-10 18:13:21 +0530

    Leetcode1493

---
+++ Leetcode1493.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_w = 0
+        num_Zeros = 0
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                num_Zeros += 1
+            while num_Zeros > 1:
+                if nums[l] == 0:
+                    num_Zeros -= 1
+                l += 1
+
+            w = r - l +1
+            max_w = max(max_w, w)
+
+        return max_w-1
